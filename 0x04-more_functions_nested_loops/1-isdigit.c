55ec70d4fdcad6262b3b73d8bc1fc2e883e18c43
joe949 <mwangiharun949@gmail.com>
2023-08-17 01:09:39 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks whether integer is a digit 0 through 9
+ * @c: integer to be checked
+ * Return: 1 if c is a digit, otherwise o.
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - print a line using character _
+ * @n: number of times the character is printed
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{		
+			_putchar('_');
+		}
+		_putchar('\n')
+	}
+}
