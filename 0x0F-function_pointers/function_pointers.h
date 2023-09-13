5d8a61b27b7f149fe9a3928d8e9544e27603c04e
joe949 <mwangiharun949@gmail.com>
2023-09-13 14:36:31 -0700

    0x0F. C - Function pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,5 +7,7 @@
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int sze, int (*cmp)(int));
+int main(int __attribute__((__unused__)) argc, char *argv[]);
+int _putchar(char c);
 
 #endif
