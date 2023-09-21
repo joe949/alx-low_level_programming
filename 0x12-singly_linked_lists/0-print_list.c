2a7df50fef7e78768c5760104e5ba8293a29f824
joe949 <mwangiharun949@gmail.com>
2023-09-21 14:46:03 -0700

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t i = 0;
+	size_t s = 0;
 
 	while (h)
 	{
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u]%s\n", h->len, h->str);
 		h = h->next;
-		i++;
+		s++;
 	}
-	return (i);
+	return (s);
 }
