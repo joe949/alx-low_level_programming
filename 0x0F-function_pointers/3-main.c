a4dded06986c2a13175f43bdbb7efaa08023801d
joe949 <mwangiharun949@gmail.com>
2023-11-08 17:45:38 -0800

    0x0F. C - Function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,11 +17,9 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][1])
-	{
-		printf("Error\n");
-		exit(99);
-	}
+
+	a = atoi(argv[1]);
+        b = atoi(argv[3]);
 
 	operation = get_op_func(argv[2]);
 
@@ -31,8 +29,6 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
 
 	printf("%d\n", operation(a, b));
 	return (0);
