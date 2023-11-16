9fceb8a72c9c4b54dd4ef903eeb34b798cee613f
joe949 <mwangiharun949@gmail.com>
2023-11-16 13:16:57 -0800

    singly linked lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,23 @@
 section .data
     hello_msg db 'Hello, Holberton', 0
+    format db '%s', 0
+    newline db 10
 
 section .text
     global main
     extern printf
 
 main:
+    ; Set up the arguments for printf
+    mov rdi, format
+    mov rsi, hello_msg
+
     ; Call printf function
-    mov rdi, hello_msg
+    call printf
+
+    ; Print a new line
+    mov rdi, format
+    mov rsi, newline
     call printf
 
     ; Clean up and exit
