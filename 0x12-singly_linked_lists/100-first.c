1d9ca159daefd10f5e02dbf09c454d45c90aa642
joe949 <mwangiharun949@gmail.com>
2023-11-16 13:00:04 -0800

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * printintro - function to be executed before main
+ *
+ * Return: void
+ */
+void printintro(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
