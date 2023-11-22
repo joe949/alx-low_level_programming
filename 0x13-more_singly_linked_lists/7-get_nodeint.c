ee202b430d9faf9e6e4434d6a0357ddf8b31592c
joe949 <mwangiharun949@gmail.com>
2023-11-22 15:11:04 -0800

    more singly linked lists

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,7 +7,7 @@
  * Return: nth node, otherewise NULL
  */
 
-listint_t *get_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int ind = 0;
 
