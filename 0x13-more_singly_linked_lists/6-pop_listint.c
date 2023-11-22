346afdf4b67efe8a25aa13a905435b8489c2ac56
joe949 <mwangiharun949@gmail.com>
2023-11-22 14:45:07 -0800

    more singly linked lists

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,16 +9,14 @@
 int pop_listint(listint_t **head)
 {
 	if (*head == NULL)
-	{
 		return (0);
-	}
 
 	int data = (*head)->n;
 
-	listint_t *tmp = *head;
+	listint_t *temp = *head;
 
 	*head = (*head)->next;
-	free(tmp);
+	free(temp);
 
 	return (data);
 }
