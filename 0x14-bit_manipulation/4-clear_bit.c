3bd139f1731cff5008510867a058b9c9ce20bbef
joe949 <mwangiharun949@gmail.com>
2023-11-23 01:05:27 -0800

    bit manipulation

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * clear_bit - function that sets the value of a bit to 0 at a given index
+ * @n: pointer to num to modify
+ * @index: index, starting from 0 of the bit you want to set
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8 || n == NULL)
+		return (-1);
+
+	mask = ~(1UL << index);
+	*n = (*n & mask);
+
+	return (1);
+}
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * flip_bits - function that returns the number of bits you
+ * would need to flip to get from one number to another
+ * @n: first num
+ * @m: second num
+ * Return: number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int  xor_res = (n ^ m);
+	unsigned int count = 0;
+
+	while (xor_res != 0)
+	{
+		count += xor_res & 1;
+		xor_res >>= 1;
+	}
+	return (count);
+}
