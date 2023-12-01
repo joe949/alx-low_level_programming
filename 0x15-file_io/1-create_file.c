d87b038c3f98bf32122d1350740bbd3868f49818
joe949 <mwangiharun949@gmail.com>
2023-12-01 01:18:06 -0800

    file handling

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file
+ * @filename: name of file created
+ * @text_content: content in file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, tl, bytesW;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, 0_CREAT | 0_WRONLY | 0_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		tl = 0;
+		while (text_content[tl] != '\0')
+			tl++;
+
+		bytesW = write(fd, text_content, tl);
+
+		if (bytesW == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
