b7a1ecf97dcf214cec35a2d7f989bea7d37eee24
joe949 <mwangiharun949@gmail.com>
2023-12-01 01:38:07 -0800

    file handling

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	tl = 0;
-	while (text_contect[tl] != '\0')
+	while (text_content[tl] != '\0')
 		tl++;
 
 	bytesW = write(fd, text_content, tl);
